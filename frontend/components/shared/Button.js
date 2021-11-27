9ba4752598300ad6cd5373e29b6409be1be370ae
Richard Westmoreland <kegtrack@gmail.com>
2021-11-27 19:23:05 +0100

    remove token from config

---
+++ frontend/components/shared/Button.js
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+
+export default function Button({
+	text,
+	disabled,
+	variant,
+	type,
+	backgroundColor,
+	textColor,
+	onClick,
+}) {
+	return (
+		<button
+			onClick={onClick}
+			className={`${variant === 'rectangular' ? 'rounded' : ''} ${
+				type === outlined
+					? 'border border-' +
+					  backgroundColor +
+					  ' bg-transparent text-shadow-of-light'
+					: 'border border-' +
+					  backgroundColor +
+					  ' bg-' +
+					  backgroundColor +
+					  ' text-' +
+					  textColor
+			} ${
+				size === 'small'
+					? 'h-8 w-16'
+					: size === 'medium'
+					? 'h-12 w-24'
+					: 'h-16 w-32'
+			}`}
+			disabled={disabled}
+		>
+			{text}
+		</button>
+	);
+}
+
+Button.defaultProps = {
+	disabled: false,
+	textColor: 'shadow-of-light',
+	variant: 'rectangular',
+	type: 'filled',
+	backgroundColor: 'light-blue',
+};
+
+Button.propTypes = {
+	text: PropTypes.string,
+	disabled: PropTypes.bool,
+	variant: PropTypes.oneOf(['rounded', 'rectangular']),
+	type: PropTypes.oneOf(['outlined', 'filled']),
+	size: PropTypes.oneOf(['small', 'medium', 'large']),
+	backgroundColor: PropTypes.string,
+	textColor: PropTypes.string,
+	onClick: PropTypes.func.isRequired,
+};
